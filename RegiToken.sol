94b70f7714a10d9e731fc9da9ce335c6dd83b4ef
Regie04 <90229394+Regie04@users.noreply.github.com>
2024-11-16 01:40:33 +0800

    Update RegiToken.sol

--- RegiToken.sol
+++ RegiToken.sol
@@ -4,16 +4,13 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Regi Token is ERC20, Ownable {
-    // Constructor to initialize the token
-    constructor() ERC20("Regi Token", "RTK") {}
+contract RegiToken is ERC20, Ownable {
+    constructor() ERC20("Regi Token", "REGI") Ownable(msg.sender) {}
 
-    // Mint function, only callable by the owner
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    // Burn function, callable by any user
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
